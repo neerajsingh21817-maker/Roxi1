df7a182ec3226fa31bda1b352a558f7946d23156
neerajsingh21817-maker <neerajsingh21817@gmail.com>
2025-08-07 14:17:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# Bot Token
+BOT_TOKEN = "8205507089:AAE8s4sZIlkH0BLvd9k6SsnSdAFj1WlQf-o"
+
+# Delivery channel numeric ID (where movie files are uploaded)
+DELIVERY_CHANNEL = -1002601317517  # @rozimovie
+
+# Message IDs of movie episodes (same for 480p and 720p in your case)
+MOVIE_MESSAGE_IDS = [5, 6, 7, 8]
+
+# Arlinks link (used for both 480p & 720p since same link)
+ARLINKS_URL = "https://go.arlinks.in/T69SS"
